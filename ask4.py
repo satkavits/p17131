e9cfd13a57f903bc7fccbf2fab59f779a507995d
ps <ps@panayiotis.0.2.15>
2018-02-02 13:38:16 +0200

    update

--- ask4.py
+++ ask4.py
@@ -1,12 +1,12 @@
 
 
 
-
+#dimiourgia sunartisis roman
 
 def roman(num):
-    
+    #lista arithmon
         numbers = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
-
+    #lista latinikon sumvolon
         romans = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
 
         i = 0
@@ -26,7 +26,11 @@ def roman(num):
 
 
 n = input('grapse enan arithmo: ') 
-try:
-    roman(int(n))
+
+try:         
+#sto pythontutor.com to exept ValueError doulevei sto pc mou bgazei NameError roman(int(n)) to opoio den gnorizo apo ti prokaleitai
+
+	roman(int(n))
+
 except ValueError:
     print('den einai arihtmos')
