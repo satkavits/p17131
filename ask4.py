a9e160ecde1c045dc7c98ca32d7ab3a9ec4cb8b3
ps <ps@panayiotis.0.2.15>
2018-02-02 13:25:21 +0200

    update

--- ask4.py
+++ ask4.py
@@ -4,22 +4,29 @@
 
 
 def roman(num):
- 
-	val = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
+    
+        numbers = [1000,900,500,400,100,90,50,40,10,9,5,4,1]
 
-	sym = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        romans = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
 
-	i = 0
+        i = 0
 
-	res = ""
+        res = ""
 
-	while num > 0:
-		for j in range(num//val[i]):
-			res += sym[i]
-			num -= val[i]
-		i += 1
-	return res
+        while num > 0:
+            for j in range(num//numbers[i]):
+                res += romans[i]
+                num -= numbers[i]
+            i += 1
+        print (res)
+
+    
+
+        
+
 
 n = input('grapse enan arithmo: ') 
-
-print(roman(n))
+try:
+    roman(int(n))
+except ValueError:
+    print('den einai arihtmos')
