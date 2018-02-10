2fc93ddba8d002f89ceb4c51d2f7d6333595e0da
ps <ps@panayiotis.0.2.15>
2018-02-10 17:55:42 +0200

    updates

---
+++ ask3.py
@@ -0,0 +1,35 @@
+
+
+
+def rot13(s):
+
+    abcl ='abcdefghijklmnopqrstuvwxyz'
+
+    abcu='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    ns=''
+
+    for char in s :
+        
+        if char in abcl:
+            p = abcl.find(char)
+            np = (p+13)%26     
+            nchar=abcl[np]
+            ns += nchar
+        elif char in abcu:
+            p = abcu.find(char)
+            np = (p+13)%26     
+            nchar=abcu[np]
+            ns += nchar 
+        else:
+            ns += char
+    return ns
+
+
+s=raw_input('grapse kati:')
+
+
+
+
+print('to kodikopoihmeno kimeno einai :',rot13(s) )
+
